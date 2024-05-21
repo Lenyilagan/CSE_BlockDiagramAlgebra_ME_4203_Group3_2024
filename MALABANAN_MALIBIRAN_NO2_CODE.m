082a39f2b8df0b596309079b19bea46bad27f1c0
KennyBhel <160506092+KennyBhel@users.noreply.github.com>
2024-05-21 17:21:33 +0800

    Update MALABANAN_MALIBIRAN_NO2_CODE.m

--- MALABANAN_MALIBIRAN_NO2_CODE.m
+++ MALABANAN_MALIBIRAN_NO2_CODE.m
@@ -37,10 +37,10 @@ close all
   %H_2 = 1/(s - 1)
   %H_3 = 1/(s - 2)
 
-G_num = [2 -6 4 0 0 0 0 0]
-G_den = [4 -8 -4 14 -8 -2 2 6 4 0 0 0 0]
+  G_num = [2 -6 4 0 0 0 0 0]
+  G_den = [4 -8 -4 14 -8 -2 2 6 4 0 0 0 0]
 
-G = tf(G_num,G_den)
+  G = tf(G_num,G_den)
 
 % step response
-step(G,0:0.1:20)
+  step(G,0:0.1:20)
