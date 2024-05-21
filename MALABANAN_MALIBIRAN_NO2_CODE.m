f22ff0aceff4efc7f3543907fe42783ef3b42b0a
KennyBhel <160506092+KennyBhel@users.noreply.github.com>
2024-05-21 17:20:17 +0800

    Update MALABANAN_MALIBIRAN_NO2_CODE.m

--- MALABANAN_MALIBIRAN_NO2_CODE.m
+++ MALABANAN_MALIBIRAN_NO2_CODE.m
@@ -30,9 +30,12 @@ close all
 %AfterReduce Block Diagram
 
 %Block Diagram Algebra
-%G_1 = 1/s^2
-%G_2 = 1/(s + 1)
-%G_3 = 1/s
-%H_1 = 1/s
-%H_2 = 1/(s - 1)
-%H_3 = 1/(s - 2)
+  %G_1 = 1/s^2
+  %G_2 = 1/(s + 1)
+  %G_3 = 1/s
+  %H_1 = 1/s
+  %H_2 = 1/(s - 1)
+  %H_3 = 1/(s - 2)
+
+G_num = [2 -6 4 0 0 0 0 0]
+G_den = [4 -8 -4 14 -8 -2 2 6 4 0 0 0 0]
